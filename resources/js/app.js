e2c5b2e5fc1b1ee5550ce4f2e230e304fb805db8
jae-hyeok02 <kjhwebd@gmail.com>
2020-08-10 23:52:41 +0900

    비주얼 영역 제작 완료

---
+++ resources/js/app.js
@@ -0,0 +1,35 @@
+
+
+const App = {
+	init() {
+		App.hook();
+	},
+
+	hook() {
+    // $(document)
+    //   .on('scroll wheel', this.scroll)
+  },
+  
+  scroll() {
+    
+  },
+}
+
+
+$(function() {
+
+  let cnt = 0;
+
+	// $(document)
+  //   .on('scroll wheel', function(e) {
+  //     console.log($(document).scrollTop());
+
+      
+      
+  //     $('html, body').stop().animate({
+  //       scrollTop: cnt * $(document).scrollTop()+'px'
+  //     }, 800);
+      
+  //   });
+
+});
